f429bbd54c05ec697ae159b4bc13089c6a6dcaf3
Muhammad Fachri <fachri@traveloka.com>
2026-01-16 22:44:06 +0700

    init aggregator and integrator service

---
+++ integrator-service/src/main/java/com/fachri/bproject/IntegratorApplication.java
@@ -0,0 +1,13 @@
+package com.fachri.bproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegratorApplication.class, args);
+	}
+
+}
