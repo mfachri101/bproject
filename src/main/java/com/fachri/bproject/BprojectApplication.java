3b4ea0d34ea4922af5060d5fbcf4a048ee3752bb
Muhammad Fachri <fachri@traveloka.com>
2026-01-16 22:18:06 +0700

    spring init

---
+++ src/main/java/com/fachri/bproject/BprojectApplication.java
@@ -0,0 +1,13 @@
+package com.fachri.bproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fachri/bproject/BprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fachri.bproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
