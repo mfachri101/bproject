90c80cd3743242619c7fbb9b61cb873e0e83aaf6
Muhammad Fachri <fachri@traveloka.com>
2026-01-18 05:51:55 +0700

    integrator: fix test

--- integrator-service/src/test/java/com/fachri/bproject/integration/abacus/AbacusNdcIntegrationImplTest.java
+++ integrator-service/src/test/java/com/fachri/bproject/integration/abacus/AbacusNdcIntegrationImplTest.java
@@ -5,23 +5,46 @@ import com.fachri.bproject.integration.pkfare.PkFareAuthServiceImpl;
 import com.fachri.bproject.integration.pkfare.PkFareIntegrationImpl;
 import com.fachri.bproject.model.FlightSearchRequest;
 import com.fachri.bproject.model.PaxNumber;
+import com.fachri.bproject.model.SeatClass;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import com.github.tomakehurst.wiremock.stubbing.StubMapping;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 import java.net.http.HttpClient;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.util.concurrent.CompletableFuture;
 
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AbacusNdcIntegrationImplTest {
+
+  // init wiremock and stubs from wiremock directory before running this test
+  @RegisterExtension
+  static WireMockExtension wm = WireMockExtension.newInstance()
+    .options(wireMockConfig()
+      .dynamicPort()
+      .withRootDirectory("../wiremock")
+    )
+    .build();
+
+  @BeforeEach
+  void setUp() {
+    for (StubMapping mapping : wm.listAllStubMappings().getMappings()) {
+      System.out.println("Stub Mapping: " + mapping.getName());
+    }
+  }
+
   @Test
   void testFetch() {
     ObjectMapper objectMapper = new ObjectMapper();
     AbacusNdcIntegrationImpl abacusNdcIntegration = new AbacusNdcIntegrationImpl(
       objectMapper,
-      "http://localhost:9090",
+      "http://localhost:" + wm.getPort(),
       HttpClient.newBuilder()
         .version(HttpClient.Version.HTTP_2)
         .connectTimeout(Duration.ofSeconds(10))
@@ -32,6 +55,7 @@ public class AbacusNdcIntegrationImplTest {
       "DPS",
       LocalDate.now().plusDays(7),
       null,
+      SeatClass.ECONOMY,
       PaxNumber.fromString("1.0.0")
     ));
 
--- integrator-service/src/test/java/com/fachri/bproject/integration/pkfare/PkFareIntegrationImplTest.java
+++ integrator-service/src/test/java/com/fachri/bproject/integration/pkfare/PkFareIntegrationImplTest.java
@@ -3,23 +3,38 @@ package com.fachri.bproject.integration.pkfare;
 import com.fachri.bproject.integration.IntegrationResponse;
 import com.fachri.bproject.model.FlightSearchRequest;
 import com.fachri.bproject.model.PaxNumber;
-import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fachri.bproject.model.SeatClass;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import com.github.tomakehurst.wiremock.stubbing.StubMapping;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 import java.net.http.HttpClient;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.util.concurrent.CompletableFuture;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class PkFareIntegrationImplTest {
 
+  @RegisterExtension
+  static WireMockExtension wm = WireMockExtension.newInstance()
+    .options(wireMockConfig()
+      .dynamicPort()
+      .withRootDirectory("../wiremock")
+    )
+    .build();
+
   @BeforeEach
   void setUp() {
-
+    for (StubMapping mapping : wm.listAllStubMappings().getMappings()) {
+      System.out.println("Stub Mapping: " + mapping.getName());
+    }
   }
 
   @Test
@@ -28,7 +43,7 @@ public class PkFareIntegrationImplTest {
     PkFareIntegrationImpl pkFareIntegration = new PkFareIntegrationImpl(
       new PkFareAuthServiceImpl(),
       objectMapper,
-      "http://localhost:9090",
+      "http://localhost:" + wm.getPort(),
       HttpClient.newBuilder()
         .version(HttpClient.Version.HTTP_2)
         .connectTimeout(Duration.ofSeconds(10))
@@ -39,6 +54,7 @@ public class PkFareIntegrationImplTest {
       "DPS",
       LocalDate.now().plusDays(7),
       null,
+      SeatClass.ECONOMY,
       PaxNumber.fromString("1.0.0")
     ));
 
